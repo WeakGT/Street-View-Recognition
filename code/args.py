b3fd5b62dcfda25168da070a0104aeca31f531bf
faidavid7 <111627118+faidavid7@users.noreply.github.com>
2024-10-16 22:53:24 +0800

    Update args.py
    exchange lr from 1e-3 to 5* 1e-5

--- code/args.py
+++ code/args.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Args:
-    lr: float = 0.001
+    lr: float = 0.00005
     val_ratio: float = 0.2
     epochs: int = 100
     batch_size: int = 32
